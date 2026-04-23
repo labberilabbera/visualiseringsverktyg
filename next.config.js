8660aaa9c96417f09c49576f851fae176d62a290
labberilabbera <labberilabbera@gmail.com>
2026-04-23 10:08:15 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,26 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: [
+              "default-src 'self'",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://ajax.googleapis.com",
+              "worker-src blob: 'self'",
+              "connect-src 'self' https://api.tripo3d.ai https://*.tripo3d.ai https://generativelanguage.googleapis.com",
+              "img-src 'self' data: blob: https://*.tripo3d.ai https://*.amazonaws.com",
+              "media-src 'self' blob: https://*.tripo3d.ai https://*.amazonaws.com",
+              "style-src 'self' 'unsafe-inline'",
+              "font-src 'self' data:",
+            ].join('; '),
+          },
+        ],
+      },
+    ];
+  },
+};
 module.exports = nextConfig;
